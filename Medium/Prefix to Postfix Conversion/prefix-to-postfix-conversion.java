10e59ef51b7efa7cfd4bdd6bf202e89318acd8a0
Kasthuri819 <155658437+Kasthuri819@users.noreply.github.com>
2024-04-09 11:06:48 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Prefix to Postfix Conversion/prefix-to-postfix-conversion.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            sc.nextLine();
+            String s = sc.next();
+            Solution obj = new Solution();
+            String ans = obj.preToPost(s);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static String preToPost(String pre_exp) {
+        
+        Stack<String> stack=new Stack<>();
+				
+		for(int i=pre_exp.length()-1;i>=0;i--)
+		{
+			char c=pre_exp.charAt(i);
+			if(isOperator(c))
+			{
+				String operand1=stack.pop();
+				String operand2=stack.pop();
+				String postfix=operand1+operand2+c;
+				stack.push(postfix);
+			}
+			else
+			{
+				stack.push(Character.toString(c));
+			}
+		}
+		
+		return stack.pop();	
+    }
+    
+    private static boolean isOperator(char c) {
+		
+		return (c=='+'||c=='-'||c=='*'||c=='/'||c=='^');
+	}
+}
