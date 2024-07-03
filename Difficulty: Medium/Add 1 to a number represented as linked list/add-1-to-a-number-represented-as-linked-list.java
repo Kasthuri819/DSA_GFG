98c6a51e729d26ae388651204f348f1fd01e167e
Kasthuri819 <155658437+Kasthuri819@users.noreply.github.com>
2024-07-03 16:09:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a number represented as linked list/add-1-to-a-number-represented-as-linked-list.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+class GfG
+{
+    public static void printList(Node node) 
+    { 
+        while (node != null)
+        { 
+            System.out.print(node.data);
+            node = node.next; 
+        }  
+        System.out.println();
+    } 
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Node head = new Node( s.charAt(0) - '0' );
+                    Node tail = head;
+                    for(int i=1; i<s.length(); i++)
+                    {
+                        tail.next = new Node( s.charAt(i) - '0' );
+                        tail = tail.next;
+                    }
+                    Solution obj = new Solution();
+                    head = obj.addOne(head);
+                    printList(head); 
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+} 
+*/
+
+class Solution
+{
+    public static Node addOne(Node head) 
+    { 
+        int carry=addRecursive(head);
+		if(carry>0)
+		{
+			Node newNode=new Node(carry);
+			newNode.next=head;
+			head=newNode;
+		}
+		return head;
+    }
+    
+    private static int addRecursive(Node head) {
+		
+		if(head==null) return 1;
+		
+		int carry=addRecursive(head.next);
+		
+		int sum=head.data+carry;
+		head.data=sum%10;
+ 		return sum/10;
+	}
+}
