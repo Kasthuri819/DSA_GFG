f0a4131af9f4ea1c694c094721900493a95a1721
Kasthuri819 <155658437+Kasthuri819@users.noreply.github.com>
2024-09-01 08:30:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
@@ -31,38 +31,28 @@ class GFG
 class Solution{
     static int matrixMultiplication(int N, int arr[])
     {
-        return matrixChainMultiplicationMemoization(N,arr);
+        int dp[][]=new int[N][N];
+		
+		for(int i=1;i<N;i++)
+		{
+			dp[i][i]=0;
+		}
+		for(int i=N-1;i>=1;i--)
+		{
+			for(int j=i+1;j<N;j++)
+			{
+				int min=Integer.MAX_VALUE;
+				for(int k=i;k<j;k++)
+				{
+					int steps=arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j];
+					
+					if(steps<min)
+						min=steps;
+					
+					dp[i][j]=min;
+				}
+			}
+		}
+		return dp[1][N-1];
     }
-    
-    private static int matrixChainMultiplicationMemoization(int N,int[] arr) {
-
-		int dp[][]=new int[N][N];
-		
-		for(int row[]:dp)
-		{
-			Arrays.fill(row,-1);
-		}
-		
-		return matrixChainMultiplicationMemo(1,N-1,arr,dp);
-	}
-
-	private static int matrixChainMultiplicationMemo(int i, int j, int[] arr, int[][] dp) {
-		
-		if(i==j) return 0;
-		
-		if(dp[i][j]!=-1) return dp[i][j];
-		
-		int min=Integer.MAX_VALUE;
-		
-		for(int k=i;k<j;k++)
-		{
-			int steps=arr[i-1]*arr[k]*arr[j]+matrixChainMultiplicationMemo(i,k, arr, dp)+
-					matrixChainMultiplicationMemo(k+1, j, arr, dp);
-			if(steps<min)
-				min=steps;
-		}
-		
-		return dp[i][j]=min;
-	}
-
 }
